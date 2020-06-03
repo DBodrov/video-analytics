138f236bcb46092d557af6bd6f930f0b7dfc25c2
Маслов Олег <ovmaslo9@mts.ru>
2020-06-04 00:21:30 +0300

    Fix mocks

--- src/backend/init-mocks.ts
+++ src/backend/init-mocks.ts
@@ -1,5 +1,9 @@
 import fetchMock from 'fetch-mock';
-import { loginMock } from './mocks/auth-mocks';
+import {
+  checkTokenMock,
+  loginMock,
+  refreshTokenMock,
+} from './mocks/auth-mocks';
 import { latestMock } from './mocks/latest-mock';
 import { locationsMock } from './mocks/locations-mock';
 import { sensorsMock } from './mocks/sensors-mock';
@@ -7,6 +11,8 @@ import { statsMock } from './mocks/stats-mock';
 import { tocsMock } from './mocks/tocs-mock';
 
 fetchMock.post('end:/api/auth/login', loginMock);
+fetchMock.post('end:/api/auth/token', refreshTokenMock);
+fetchMock.get('end:/api/auth/check-token', checkTokenMock);
 
 fetchMock.get('express:/api/va/companies/:companyId/locations', locationsMock);
 fetchMock.get('express:/api/va/companies/:companyId/sensors', sensorsMock);
