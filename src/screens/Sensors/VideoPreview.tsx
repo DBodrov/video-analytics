0ca00536f9c7cfc33b1ac63802705b4e436e3cd7
D.Bodrov <D.Bodrov@otpbank.ru>
2020-10-16 23:57:17 +0300

    save gen url

--- src/screens/Sensors/VideoPreview.tsx
+++ src/screens/Sensors/VideoPreview.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import {CompanySensorGetResponse200Status} from '@/backend/main';
+import {TEST_HOST} from '@/utils';
 import {Tag} from './styles';
 
 type TVideoPreviewProps = {
@@ -9,16 +10,22 @@ type TVideoPreviewProps = {
 };
 
 const statusColor: Record<string, string> = {
-  'in_use': '#36AF47',
-  'inactive': '#F7981C',
-  'error': '#F95359',
-}
+  in_use: '#36AF47',
+  inactive: '#F7981C',
+  error: '#F95359',
+};
 
 export function VideoPreview({companyId, sensorId, status}: TVideoPreviewProps) {
+  const isTestHost = window.location.host === TEST_HOST;
+  const url = isTestHost
+    ? `http://9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6${sensorId}.video.dev-va-0002.msk.mts.ru/api/video/companies/${companyId}/sensors/${sensorId}`
+    : `/api/video/companies/${companyId}/sensors/${sensorId}`;
   return (
     <div css={{width: 338, height: 208, position: 'relative'}}>
-      <img src={`/api/video/companies/${companyId}/sensors/${sensorId}`} alt="" width="100%" height="100%" />
-      <Tag css={{position: 'absolute', bottom: 10, left: 10, backgroundColor: statusColor[status.code]}}>{status.name}</Tag>
+      <img src={url} alt="" width="100%" height="100%" />
+      <Tag css={{position: 'absolute', bottom: 10, left: 10, backgroundColor: statusColor[status.code]}}>
+        {status.name}
+      </Tag>
     </div>
   );
 }
