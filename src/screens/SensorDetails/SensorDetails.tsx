56b8743bbadcab54b42ee16cfdae497e21b86b15
Бодров Дмитрий Михайлович <dmbodro1@mts.ru>
2020-11-03 21:51:10 +0300

    Update api

--- src/screens/SensorDetails/SensorDetails.tsx
+++ src/screens/SensorDetails/SensorDetails.tsx
@@ -1,19 +1,35 @@
 import React from 'react';
 import {useParams} from 'react-router-dom';
 import {v4 as uuidv4} from 'uuid';
-import {useEvents, TEventView, useAuth} from '@/context';
+import {Button} from 'neutrino-ui';
+import {useAuth} from '@/context';
+import {useEventsClient} from '@/context/Events/use-events-client';
 import {BoxToggle} from '@/components';
 import {HOST} from '@/utils';
 import {AppLayout} from '../Layouts';
 import {EventsTable} from '../Events/EventsTable';
+import {TIMEZONE_OFFSET} from '@/utils';
+import {DayStats, TechInfo, ActiveChecks} from './components';
 import {CameraBox, CameraInfoBox} from './styles';
 
+const allDay = () => {
+  let startTime = new Date();
+  startTime.setHours(0, 0, 0, 0);
+  const isoStart = startTime.toISOString();
+  let endTime = new Date();
+  endTime.setHours(23, 59, 59, 999);
+  const isoEnd = endTime.toISOString();
+  return {
+    beginDay: isoStart,
+    endDay: isoEnd,
+  }
+};
+
 export function SensorDetails() {
-  const [events, setEvents] = React.useState<TEventView[] | undefined>(undefined);
   const [showBoxes, setShowBoxes] = React.useState(true);
   const {companyId} = useAuth();
   const {id} = useParams<{id: string}>();
-  const {getEventsViewBySensorId, isError, isIdle, isLoading, isSuccess, error} = useEvents();
+  const {queryEvents, isError, isIdle, isLoading, isSuccess, error, eventsView} = useEventsClient();
   const videoUrl = React.useMemo(
     () =>
       `http://${uuidv4()}.video.${HOST}/api/live/company/${companyId}/sensors/${id}${
@@ -22,13 +38,13 @@ export function SensorDetails() {
     [companyId, id, showBoxes],
   );
 
-  //TODO: Убрать внутрь EventsTable, в отдельный client-hook
   React.useEffect(() => {
-    if (!events && Number(id)) {
-      const eventsView = getEventsViewBySensorId(Number(id))?.slice(0, 50);
-      setEvents(eventsView);
+    if (eventsView?.length === 0 && !isSuccess) {
+      const today = allDay();
+      queryEvents({sensorIds: Number(id), tzOffset: TIMEZONE_OFFSET, startTime: today.beginDay, endTime: today.endDay, page: 1, pageSize: 50});
     }
-  }, [events, getEventsViewBySensorId, id]);
+  }, [eventsView, id, isSuccess, queryEvents]);
+
   return (
     <AppLayout>
       <div css={{width: '100%', height: '100%', padding: '30px 30px'}}>
@@ -38,7 +54,14 @@ export function SensorDetails() {
             <img src={videoUrl} alt="" width="100%" height="100%" />
           </div>
           <CameraInfoBox>
-            <div>Info</div>
+            <TechInfo />
+            <DayStats />
+            <ActiveChecks />
+            <div css={{paddingLeft: 30}}>
+              <Button flat variant="primary" css={{width: 170, fontSize: 14}}>
+                Редактировать камеру
+              </Button>
+            </div>
           </CameraInfoBox>
         </CameraBox>
         <EventsTable
@@ -47,7 +70,7 @@ export function SensorDetails() {
           isLoading={isLoading}
           isSuccess={isSuccess}
           error={error}
-          eventsView={events}
+          eventsView={eventsView}
         />
       </div>
     </AppLayout>
---
+++ src/screens/SensorDetails/components/TechInfo.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import {Span} from 'neutrino-ui';
+
+export function TechInfo() {
+  return <div css={{paddingLeft: 30, backgroundColor: 'var(--color-border)'}}>
+    <Span css={{fontSize: 12, color: 'var(--color-text-secondary)'}}>Много букв</Span>
+  </div>
+}
